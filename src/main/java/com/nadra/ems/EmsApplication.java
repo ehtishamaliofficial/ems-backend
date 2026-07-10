21c9c4c924eac506bf412fd01a1d049f3e96e3d3
Ehtisham Ali <ehtisham.rajpoot.official@gmail.com>
2026-07-11 02:09:45 +0500

    first commit

---
+++ src/main/java/com/nadra/ems/EmsApplication.java
@@ -0,0 +1,13 @@
+package com.nadra.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(EmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nadra/ems/EmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadra.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
